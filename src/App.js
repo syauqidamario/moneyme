c50e682d5a2ca48eda0238de69288169a6a0aceb
syauqidamario <syauqi.djohan@student.umn.ac.id>
2023-11-09 16:25:06 +0700

    Added CSS to the website.

--- src/App.js
+++ src/App.js
@@ -9,61 +9,74 @@ class App extends React.Component{
   render() {
     return (
       <>
-        <div className='container'>
+        <div className='container py-5'>
           <div className='row'>
-            <div className='col-12'>
-              <h1>MoneyMe Apps</h1>
-              <hr />
-              <>
-              <h4>Rp. 2.500.000,-</h4>
+            <div className='col-12 text-center'>
+              <h1 className='fw-bold'>MoneyMe Apps</h1>
+              <hr className='w-75 mx-auto' />
+              <h2 className='fw-bold'>Rp. 2.500.000,-</h2>
               <span className='title'>You have 75% money left</span>
-              </>
             </div>
           </div>
 
-          <div className="row">
-            <div className='row'>
-              <div className='card-wrapper'>
-              <div className='icon-wrapper'>
-                <i class="bi bi-wallet2"></i>
+          <div className="row mt-4">
+            <div className='col-6'>
+              <div className='card-wrapper p-4'>
+              <div className='icon-wrapper mb-1'>
+                <i class="bi bi-cash-stack"></i>
               </div>
               <span className="title">
                 Income
               </span>
-              <h3>Rp. 2.500.000</h3>
+              <h3 className='fw-bold '>Rp. 2.500.000</h3>
               <div>
-                <span className='title'>50</span><span className='title'> Transactions</span>
+                <span className='title-sm text-indigo fw-bold'>50</span><span className='title-sm'> Transactions</span>
               </div>
               </div>
             </div>
 
-            <div className='row'>
+            <div className='col-6'>
+              <div className='card-wrapper p-4'>
+              <div className='icon-wrapper mb-1'>
+                <i class="bi bi-cash-stack"></i>
+              </div>
+              <span className="title">
+                Income
+              </span>
+              <h3 className='fw-bold '>Rp. 2.500.000</h3>
+              <div>
+                <span className='title-sm text-indigo fw-bold'>50</span><span className='title-sm'> Transactions</span>
+              </div>
+              </div>
+            </div>
+          </div>
+
+            <div className='row mt-5'>
               <div className='col-12 d-flex justify-content-between align-items-center'>
-                <h2>Summary of Transactions</h2>
+                <h4>Summary of Transactions</h4>
                 <div className='wrapper-button d-flex'></div>
-                  <button>Income <i class="bi bi-plus-circle-fill"></i></button>
-                  <button>Expenses <i class="bi bi-dash-circle-fill"></i></button>
+                  <button className='button btn-indigo px-3 py-2 me-2'>Income <i class="bi bi-plus-circle-fill"></i></button>
+                  <button className='button btn-red px-3 py-2'>Expenses <i class="bi bi-dash-circle-fill"></i></button>
               </div>
             </div>
 
-            <div className='row'>
+            <div className='row mt-4'>
               <div className='col-12 d-flex justify-content-between align-items-center'>
                 <div className='d-flex align-items-center'>
                   <div className='icon-wrapper'>
                     <i class="bi bi-wallet2"></i>
                   </div>
 
-                  <div className="transaction">
+                  <div className="transaction ms-3 d-flex flex-column">
                     <h6>Pay day</h6>
-                    <span className='title'>October 25 2023</span>
+                    <span className='title-sm'>October 25 2023</span>
                 </div>
                 </div>
 
-                <h5 className='money'>Rp.6.000.000</h5>
+                <h5 className='text-money-in'>Rp.6.000.000</h5>
               </div>
             </div>
           </div>
-        </div>
       </>
     )
   }
